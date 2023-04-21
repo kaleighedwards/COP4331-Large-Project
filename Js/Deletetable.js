f8eead2dd35a65f7e8196f55e3b5247adaa3bd43
Matthew Sterling <123518768+MatthewSterlin@users.noreply.github.com>
2023-04-21 00:38:20 -0400

    Added delete button functionality

--- Js/Deletetable.js
+++ Js/Deletetable.js
@@ -1,13 +1,5 @@
-DeleteBtn = document.getElementById("delete")
-
-DeleteBtn.on('click', function() {
-    DeleteRow(DeleteBtn);
+$(document).ready(function(){
+    $('table').on('click', '.btn-danger', function(){
+        $(this).closest('tr').remove();
+    });
 });
-
-function DeleteRow(btndel) {
-    if (typeof(btndel) == "object") {
-        $(btndel).closest("tr").remove();
-    } else {
-        return false;
-    }
-}
