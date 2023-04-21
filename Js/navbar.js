732016bcd5ece9ba171756942b9b46d957e0fc13
Matthew Sterling <123518768+MatthewSterlin@users.noreply.github.com>
2023-04-21 18:08:55 -0400

    Added Login/Signup Modal

--- Js/navbar.js
+++ Js/navbar.js
@@ -1,7 +1,7 @@
 const Reservations = document.getElementById("Reservations");
 const Home = document.getElementById("Quest");
 const Login = document.getElementById("Login");
-
+const Signup = document.getElementById("Signup")
 Reservations.addEventListener("click", () => {
     window.location.href = "reservations.html";
 });
@@ -10,8 +10,22 @@ Home.addEventListener("click", () => {
     window.location.href = "index.html";
 });
 
+/* Old code for previous login page
 Login.addEventListener("click", () => {
     window.location.href = "login.html";
 });
+*/
+//Shows the login modal when pressed via JQuery 
+$(document).ready(function() {
+    $("#Login").click(function() {
+      $("#loginModal").modal('show');
+    });
+  });
+
+  $(document).ready(function() {
+    $("#Signup").click(function() {
+      $("#SignupModal").modal('show');
+    });
+  });
 
 
