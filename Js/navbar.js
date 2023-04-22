43d2ff2ca95257f2d453e3e49a310fd9d35f24d7
Matthew Sterling <123518768+MatthewSterlin@users.noreply.github.com>
2023-04-22 19:59:29 -0400

    Stores signup User/pass

--- Js/navbar.js
+++ Js/navbar.js
@@ -2,6 +2,8 @@ const Reservations = document.getElementById("Reservations");
 const Home = document.getElementById("Quest");
 const Login = document.getElementById("Login");
 const Signup = document.getElementById("Signup")
+const RegisterForm = document.getElementById("RegisterForm");
+
 Reservations.addEventListener("click", () => {
     window.location.href = "reservations.html";
 });
@@ -27,5 +29,40 @@ $(document).ready(function() {
       $("#SignupModal").modal('show');
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('#RegisterForm');
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+      const formData = new FormData(form);
+      let email = formData.get('email');
+      let signupPass = formData.get('signupPass');
+      let confirmPass = formData.get('confirmPass');
+      console.log("Email: ", email);
+      console.log("Password1: ", signupPass);
+      console.log("Password2: ", confirmPass);
+
+      const axios = require('axios');
+      
+      axios.post('http://questelectronics.store/signup', {
+        Username: 'email',
+        Password: 'signupPass',
+        PermLvl: 1
+    })
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.error(error.response.data);
+    });
+    });
+
+  });
+  
+  
+  
+
+  
+
 
 
