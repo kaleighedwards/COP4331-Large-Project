5b5df4e67c31c532fee8f7dae5b3bdbb8e16b585
Matthew Sterling <123518768+MatthewSterlin@users.noreply.github.com>
2023-04-23 19:01:10 -0400

    testing API endpoint

--- Js/navbar.js
+++ Js/navbar.js
@@ -4,60 +4,9 @@ const Login = document.getElementById("Login");
 const Signup = document.getElementById("Signup")
 const RegisterForm = document.getElementById("RegisterForm");
 
-document.addEventListener('DOMContentLoaded', function() {
-  const LoginForm = document.querySelector('#LoginForm')
-  const Signupform = document.querySelector('#RegisterForm');
-
-
-  Signupform.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const formData = new FormData(Signupform);
-    let email = formData.get('email');
-    let signupPass = formData.get('signupPass');
-    let confirmPass = formData.get('confirmPass');
-    console.log("Email: ", email);
-    console.log("Password1: ", signupPass);
-    console.log("Password2: ", confirmPass);
-
-
-    const cors = require('cors');
-    axios.post('http://localhost:5500/signup', {
-      Username: email,
-      Password: signupPass,
-      PermLvl: 2
-  })
-  .then(response => {
-      console.log(response.data);
-  })
-  .catch(error => {
-      console.error(error.response.data);
-  });
-  });
-
-
-  LoginForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const formData = new FormData(LoginForm);
-    let email = formData.get('email');
-    let password = formData.get('password');
-    console.log("Email: ", email);
-    console.log("Password1: ", password);
 
 
 
-    axios.post('http://localhost:5500/signin', {
-      Username: email,
-      Password: password,
-  })
-  .then(response => {
-      console.log(response.data);
-  })
-  .catch(error => {
-      console.error(error.response.data);
-  });
-  });
-
-});
 
 
 Reservations.addEventListener("click", () => {
@@ -86,7 +35,61 @@ $(document).ready(function() {
     });
   });
 
+  document.addEventListener('DOMContentLoaded', function() {
+    const LoginForm = document.querySelector('#LoginForm')
+    const Signupform = document.querySelector('#RegisterForm');
 
+
+    Signupform.addEventListener('submit', function(e) {
+      e.preventDefault();
+      const formData = new FormData(Signupform);
+      let email = formData.get('email');
+      let signupPass = formData.get('signupPass');
+      let confirmPass = formData.get('confirmPass');
+      console.log("Email: ", email);
+      console.log("Password1: ", signupPass);
+      console.log("Password2: ", confirmPass);
+
+
+      axios.post('http://127.0.0.1:5500/Large%20project/Js/auth', {
+        Username: email,
+        Password: signupPass,
+        PermLvl: 2,
+        
+
+    },{withCreditentials: true})
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.error(error.response.data);
+    });
+    });
+
+
+    LoginForm.addEventListener('submit', function(e) {
+      e.preventDefault();
+      const formData = new FormData(LoginForm);
+      let email = formData.get('email');
+      let password = formData.get('password');
+      console.log("Email: ", email);
+      console.log("Password1: ", password);
+
+
+
+      axios.post('http://localhost:5500/signin', {
+        Username: email,
+        Password: password,
+    })
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.error(error.response.data);
+    });
+    });
+
+  });
   
   
   
