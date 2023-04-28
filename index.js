200b695f980e17dcf2d3846f62cfcacdf89c72d8
Glenroy Little <84647047+GSabiniPanini@users.noreply.github.com>
2023-04-27 22:40:36 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+require('dotenv').config();
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri).then(() => console.log("Mongo DB connected")).catch(err => console.log(err));
+
+var api = require('./Js/api');
+api.setApp( app, mongoose );
+
+const PORT = process.env.PORT || 3000;
+app.listen(
+    // Lets us know that the server is running
+    PORT,
+    () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    }
+);
+
