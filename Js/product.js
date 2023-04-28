26d34b343420e66ab83e019ba08238387fa04763
Glen <g.little02@outlook.com>
2023-04-28 07:54:10 -0400

    All endpoints work now localhost

--- Js/product.js
+++ Js/product.js
@@ -4,26 +4,30 @@ require('mongodb');
 exports.productRouter = function (app, productCollection, userCollection) {
     // add to stock endpoint
     app.post('/api/addstock', async (req, res, next) => {
-        const { UserID, Name, Amt } = req.body;
+        const { Username, Name, Amt } = req.body;
 
         try {
             //check if user PermLvl is admin (PermLvl < 2)
-            const user = await userCollection.findOne({ _id: UserID });
+            const user = await userCollection.findOne({ Username });
 
-            if (user.PermLvl === 1) {
-                //check if product exists
-                const item = await productCollection.findOne({ Name });
+            if (user) {
+                if (user.PermLvl === 1) {
+                    //check if product exists
+                    const item = await productCollection.findOne({ Name });
 
-                if (item) {
-                    //update product
-                    const result = await productCollection.updateOne({ Name }, { $inc: { Amt } });
-
-                    res.status(201).json({ message: Amt + ' stock has been added to ' + item.Name + ' successfully' });
+                    if (item) {
+                        //update product
+                        const result = await productCollection.updateOne({ Name }, { $inc: { Amt } });
+    
+                        res.status(201).json({ message: Amt + ' stock has been added to ' + item.Name + ' successfully' });
+                    } else {
+                        res.status(404).json({ message: 'Item not found' });
+                    }
                 } else {
-                    res.status(404).json({ message: 'Item not found' });
+                    res.status(403).json({ message: 'This ID does not have permission to add to stock' });
                 }
             } else {
-                res.status(403).json({ message: 'This ID does not have permission to add to stock' });
+                res.status(404).json({ message: 'User not found' });
             }
         }
         catch (err) {
