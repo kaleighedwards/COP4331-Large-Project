41318b43f593ebb57957588f80201febad4585e3
MatthewSterlin <Matthew.Sterling@knights.ucf.edu>
2023-04-28 01:42:09 -0400

    Added API call for reserve item

--- Js/ReserveItem.js
+++ Js/ReserveItem.js
@@ -9,10 +9,28 @@
       });
   } 
 
+  //cookie decoder
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
   $(document).ready(function(){
     //Gets the Name of the item  and the amt
     let amt = 1;
-    let userID  = 1;   
+    const _Id = getCookie("_Id");
+  
     let table_amt = $(this).closest('tr').children('td').eq(2).text();
     let stock_checker = 0;
     $('table').on('click', '.btn-success', function(){
@@ -36,9 +54,16 @@
       let data = {
         ItemID: name,
         ItemAmt: amt,
-        userID: userID
+        _Id: _Id
       }
       console.log(data);
+      axios.post('https://questelectronics.store/api/reserve', data)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error?.response?.data);
+      });
     });
 
 });
