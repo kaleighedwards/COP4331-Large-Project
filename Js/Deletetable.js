495cbc7ac61d28505e6f600662d273c6c3381a65
MatthewSterlin <Matthew.Sterling@knights.ucf.edu>
2023-04-28 15:17:56 -0400

    fixed undefined values

--- Js/Deletetable.js
+++ Js/Deletetable.js
@@ -15,21 +15,7 @@ function getCookie(cname) {
     return "";
   }
   //function that takes the users reservations and builds a table
-  function buildTable(data){
-    var table = $('#dtBasicExample').DataTable();  
-    for (var i = 0; i < data.length; i++){
-      var row = `<tr>
-                    <td></td>
-                    <td>${data[i].ItemName}</td>
-                    <td>${data[i].ItemAmt}</td>
-                    <td>
-                    <button id = "delete" class="btn btn-danger btn-sm"><i class="fas fa-times"></i></button>    
-                  </td>
-                </tr>`
-      table.row.add($(row)).draw();
-    }
-    makeTableSortable();
-}
+
 
   //gets the users reservation table
 $(document).ready(function(){
@@ -59,7 +45,6 @@ $(document).ready(function(){
     axios.get('https://questelectronics.store/api/reserve/' + "Ben10@alien.com", "")
     .then(response => {
       console.log(response.data);
-      buildTable(response.data);
     })
     .catch(error => {
       console.log(error?.response?.data);
